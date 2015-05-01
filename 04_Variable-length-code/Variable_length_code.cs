e475946b2af22449677563869f7aa9f8b7e70189
nstanevski <nikolay.stanevski@gmail.com>
2015-05-01 21:07:56 +0300

    task#4 added

---
+++ 04_Variable-length-code/Variable_length_code.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Variable_length_code
+{
+    static void Main()
+    {
+        //process input
+        string[] encChars = Console.ReadLine().Split(' ');
+        List<int> encNums = new List<int>();
+        for (int i = 0; i < encChars.Length; i++)
+        {
+            if (encChars[i].Length == 0)
+                continue;
+            encNums.Add(int.Parse(encChars[i]));
+        }
+        int numMapEntries = int.Parse(Console.ReadLine());
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        for (int i = 0; i < numMapEntries; i++)
+        {
+            String line = Console.ReadLine();
+            char ch = line[0];
+            int numOnes = int.Parse(line.Substring(1));
+            map.Add(ch, numOnes);
+        }
+
+        //decrypt
+        StringBuilder encMsg = new StringBuilder();
+        foreach (int encNum in encNums)
+        {     
+            string binCharStr = Convert.ToString(encNum, 2);
+            encMsg.Append(binCharStr);
+            //Console.WriteLine(binCharStr);
+        }
+        //remove trailing zeroes:
+        while (encMsg[encMsg.Length - 1] == '0')
+            encMsg.Remove(encMsg.Length - 1, 1);
+
+        string[] onesArr = encMsg.ToString().Split('0');
+        Console.WriteLine(encMsg);
+        foreach (string onesEntry in onesArr)
+        {
+            //Console.WriteLine(onesEntry);
+        }
+        
+           // Console.WriteLine("End");
+
+
+    }
+}
