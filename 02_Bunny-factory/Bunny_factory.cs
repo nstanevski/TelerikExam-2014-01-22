72daa1b302affb844ba37f05b8fabe56c9fbd294
nstanevski <nikolay.stanevski@gmail.com>
2015-05-01 15:24:52 +0300

    Task#2 finalized

--- 02_Bunny-factory/Bunny_factory.cs
+++ 02_Bunny-factory/Bunny_factory.cs
@@ -9,17 +9,58 @@ class Bunny_factory
     {
         List<int> factory = new List<int>();
         string input = "";
+
         while ((input = Console.ReadLine()) != "END")
         {
             factory.Add(int.Parse(input));
         }
-        //foreach (int cage in factory)
-        //{
-        //    Console.WriteLine(cage);
-        //}
+        
+        int cycleNum = 1;
         while (true)
         {
+            int numCages = 0;
+            int factoryLen = factory.Count;
 
+            for (int i = 0; i < cycleNum; i++)
+                numCages += factory[i];
+
+            if (factoryLen - cycleNum < numCages)
+                break;
+
+            int sum = 0;
+            BigInteger product = 1;
+
+            for (int i = cycleNum; i < numCages+cycleNum; i++)
+            {
+                sum += factory[i];
+                product *= factory[i];
+            }
+
+            //string buffer holding newly created cages
+            StringBuilder sb = new StringBuilder(sum.ToString() + product.ToString());
+            //append untouched cages
+            for (int i = numCages+cycleNum; i < factoryLen; i++)
+                sb.Append(factory[i]);
+
+            //load the new factory
+            factory.Clear();
+            char[] digits = sb.ToString().ToCharArray();
+           
+            for (int i = 0; i < digits.Length; i++)
+            {
+                int digit = (int)digits[i] - (int)'0';
+                if (digit > 1)
+                    factory.Add(digit);
+            }
+            cycleNum++;
         }
+
+        //print output result
+        for (int i = 0; i < factory.Count; i++)
+        {
+            Console.Write(factory[i]);
+            if (i < factory.Count - 1)
+                Console.Write(" ");
+        }  
     }
 }
