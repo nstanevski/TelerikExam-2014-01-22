614f9f46a9c9a9662625f7edf03cc6b19090d90f
nstanevski <nikolay.stanevski@gmail.com>
2015-05-01 13:17:02 +0300

    Task#2 - input part done

--- 02_Bunny-factory/Bunny_factory.cs
+++ 02_Bunny-factory/Bunny_factory.cs
@@ -1,9 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
 
 class Bunny_factory
 {
     static void Main()
     {
+        List<int> factory = new List<int>();
+        string input = "";
+        while ((input = Console.ReadLine()) != "END")
+        {
+            factory.Add(int.Parse(input));
+        }
+        //foreach (int cage in factory)
+        //{
+        //    Console.WriteLine(cage);
+        //}
+        while (true)
+        {
+
+        }
     }
 }
