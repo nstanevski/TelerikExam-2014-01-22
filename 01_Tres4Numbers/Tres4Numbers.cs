da0c13d16fdcf0ebf1b5b00bc4cc33a6b5fc8574
nstanevski <nikolay.stanevski@gmail.com>
2015-05-01 12:02:38 +0300

    Initially added to source control

---
+++ 01_Tres4Numbers/Tres4Numbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    class Tres4Numbers
+    {
+        static void Main()
+        {
+            string[] tres4Digits = { "LON+","VK-","*ACAD","^MIM","ERIK|","SEY&","EMY>>","/TEL","<<DON"};
+        }
+    }
